b5911403047a23c7741d37864eb0135663b188a6
fmuller <fmuller@unice.fr>
2016-11-05 14:04:04 +0100

    Test

---
+++ src/java_projet_test/MonTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package java_projet_test;
+
+/**
+ * @author fmuller
+ *
+ */
+public class MonTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Mon test");
+	}
+
+}
